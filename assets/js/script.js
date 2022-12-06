5943dd2d5c4ac58d0cb549118ebd9e3af11b29af
Igor Oliveira de Sousa <igor.oliveira@rfsolucoes.local>
2022-12-06 12:03:49 -0300

    Estrutura Tabuada

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+firstNum = document.getElementById('first-number');
+secondNum = document.getElementById('second-number');
+
+function calcular() {
+    for (let i = 0; i < secondNum; i++) {
+        return firstNum = firstNum * i;
+    }
+}
+
